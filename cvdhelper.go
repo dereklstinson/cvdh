8683a38912f8eda7741a07e0900da7ef44883464
Derek Stinson <dereklstinson@gmail.com>
2018-12-17 17:43:43 -0500

    added a couple funcs

---
+++ cvdhelper.go
@@ -0,0 +1,69 @@
+package cvdhelper
+
+import (
+	"errors"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+//GetPaths will return the paths of all the files in the directory and sub directorys. If suffixes is nil then it will grab everything.
+func GetPaths(folder string, withsuffixes []string) (paths []string, err error) {
+	paths = make([]string, 0)
+	err = filepath.Walk(folder, func(path string, info os.FileInfo, errw error) error {
+		if errw != nil {
+			return errw
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if withsuffixes == nil {
+			paths = append(paths, path)
+		} else {
+			for i := range withsuffixes {
+				if strings.HasSuffix(path, withsuffixes[i]) {
+					paths = append(paths, path)
+				}
+
+			}
+		}
+
+		return nil
+	})
+	return paths, err
+}
+
+//GetImageHD will return an image.Image based on the prefix.  Supported files are .jpeg, jpg, png.
+func GetImageHD(path string) (image.Image, error) {
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		return nil, err
+	}
+	if strings.HasSuffix(path, ".jpeg") || strings.HasSuffix(path, ".jpg") {
+		return jpeg.Decode(file)
+	} else if strings.HasSuffix(path, ".png") {
+		return png.Decode(file)
+	}
+	return nil, errors.New("Unsupported Format")
+}
+
+/*
+//GetImage takes an io.Reader and first checks to see if it is a jpeg. If not it will check to see if it is an png.
+func GetImage(r io.Reader) (image.Image, error) {
+	img, err1 := jpeg.Decode(r)
+	if err1 != nil {
+		strerr := err1.Error()
+		img1, err2 := png.Decode(r)
+		if err2 != nil {
+			strerr2 := err2.Error()
+			return nil, errors.New("JPEG: " + strerr + ", PNG: " + strerr2 + " ---Unsupported Format")
+		}
+		return img1, err2
+	}
+	return img, err1
+}
+*/
---
+++ cvdhelper_test.go
@@ -0,0 +1,90 @@
+package cvdhelper_test
+
+import (
+	"testing"
+
+	"github.com/dereklstinson/cvdhelper"
+)
+
+func TestGetPaths(t *testing.T) {
+	paths, err := cvdhelper.GetPaths("./testimgs/", []string{".png"})
+	if err != nil {
+		t.Error(err)
+	}
+	if len(paths) != 3 {
+		t.Error("Should Be three")
+	}
+	paths, err = cvdhelper.GetPaths("./testimgs/", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(paths) != 5 {
+		t.Error("Should Be five")
+	}
+	paths, err = cvdhelper.GetPaths("./testimgs/", []string{".jpg"})
+	if err != nil {
+		t.Error(err)
+	}
+	if len(paths) != 1 {
+		t.Error("Should Be one")
+	}
+	paths, err = cvdhelper.GetPaths("./testimgs/", []string{".jpg", ".png"})
+	if err != nil {
+		t.Error(err)
+	}
+	if len(paths) != 4 {
+		t.Error("Should Be four")
+	}
+}
+
+func TestGetImageHD(t *testing.T) {
+	paths, err := cvdhelper.GetPaths("./testimgs/", []string{".jpg", ".png"})
+	if err != nil {
+		t.Error(err)
+	}
+	for _, path := range paths {
+		img, err := cvdhelper.GetImageHD(path)
+		if err != nil {
+			t.Error(err)
+		}
+		if img == nil {
+			t.Error("Shouldn't Be nil")
+		}
+	}
+	paths, err = cvdhelper.GetPaths("./testimgs/", []string{".mat"})
+	if err != nil {
+		t.Error(err)
+	}
+	for _, path := range paths {
+		img, err := cvdhelper.GetImageHD(path)
+		if err == nil {
+			t.Error("This should have been nil")
+		}
+		if img != nil {
+			t.Error("This should be nil")
+		}
+	}
+}
+
+/*
+func TestGetImage(t *testing.T) {
+	paths, err := cvdhelper.GetPaths("./testimgs/", []string{".jpg", ".png"})
+	if err != nil {
+		t.Error(err)
+	}
+	for _, path := range paths {
+		file, err := os.Open(path)
+		defer file.Close()
+		if err != nil {
+			t.Error(err)
+		}
+		img, err := cvdhelper.GetImage(file)
+		if err != nil {
+			t.Error(err)
+		}
+		if img == nil {
+			t.Error("Shouldn't Be nil")
+		}
+	}
+}
+*/
