470e550cf2b0d1f1d12d8c7e8edbb1dbb8475822
Derek Stinson <dereklstinson@gmail.com>
2018-12-18 18:28:53 -0500

    fixed change

--- cvdhelper.go
+++ cvdhelper.go
@@ -2,61 +2,6 @@ package cvdhelper
 
 import (
 	"errors"
-	"image"
-	"image/jpeg"
-	"image/png"
-	"os"
-	"path/filepath"
-	"strings"
-)
-
-//GetPaths will return the paths of all the files in the directory and sub directorys. If suffixes is nil then it will grab everything.
-func GetPaths(folder string, withsuffixes []string) (paths []string, err error) {
-	paths = make([]string, 0)
-	err = filepath.Walk(folder, func(path string, info os.FileInfo, errw error) error {
-		if errw != nil {
-			return errw
-		}
-		if info.IsDir() {
-			return nil
-		}
-		if withsuffixes == nil {
-			paths = append(paths, path)
-		} else {
-			for i := range withsuffixes {
-				if strings.HasSuffix(path, withsuffixes[i]) {
-					paths = append(paths, path)
-				}
-
-			}
-		}
-
-		return nil
-	})
-	return paths, err
-}
-
-//GetImageHD will return an image.Image based on the prefix.  Supported files are .jpeg, jpg, png.
-func GetImageHD(path string) (image.Image, error) {
-	file, err := os.Open(path)
-	defer file.Close()
-	if err != nil {
-		return nil, err
-	}
-	if strings.HasSuffix(path, ".jpeg") || strings.HasSuffix(path, ".jpg") {
-		return jpeg.Decode(file)
-	} else if strings.HasSuffix(path, ".png") {
-		return png.Decode(file)
-	}
-	return nil, errors.New("Unsupported Format")
-}
-
-/*
-
-package cvdhelper
-
-import (
-	"errors"
 	"fmt"
 	"image"
 	"image/jpeg"
@@ -192,7 +137,7 @@ func channelsitter(imgs []image.Image, imgchan <-chan image.Image, wg *sync.Wait
 	wg.Done()
 }
 
-
+/*
 //GetImage takes an io.Reader and first checks to see if it is a jpeg. If not it will check to see if it is an png.
 func GetImage(r io.Reader) (image.Image, error) {
 	img, err1 := jpeg.Decode(r)
